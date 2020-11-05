af1881df2ae4e82ebd5cda828d1a00f15b0702c0
Miguel Varona <mavaronad@gmail.com>
2020-11-05 22:38:12 +0100

    Added docs

---
+++ docs/precache-manifest.433e301d60da41fb6c96c7f11d055087.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "903738a4d4599eafcc7c3ee3c5c2f197",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb4b27a4d4634905b3f1",
+    "url": "/static/css/main.63b07e35.chunk.css"
+  },
+  {
+    "revision": "981650a3b6d2d8451b72",
+    "url": "/static/js/2.ae99edc5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.ae99edc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb4b27a4d4634905b3f1",
+    "url": "/static/js/main.b16c4605.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
