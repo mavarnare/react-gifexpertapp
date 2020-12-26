5342e1cd67d89725e224c2fa6a3f9f8608c0be0e
Mavarnare <mavaronad@gmail.com>
2020-12-26 15:31:56 +0100

    Test for 'useFetchGifs' hook

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -2,13 +2,27 @@ import { renderHook } from "@testing-library/react-hooks";
 import { useFetchGifs } from "../../hooks/useFetchGifs";
 
 describe('Pruebas para el hook useFetchGifs', () => {
-    test('Debe retornar el estado inicial', () => {
+
+    
+
+    test('Debe retornar el estado inicial', async () => {
         const category = 'Dragon Ball';
-        const { result } = renderHook( () => useFetchGifs(category) );
+        const { result , waitForNextUpdate } = renderHook( () => useFetchGifs(category) );
         const { data, loading } = result.current;
-        
+        await waitForNextUpdate();
+
         expect( data ).toEqual([]);
         expect( loading ).toBe(true);
+    })
+    
+    test('debe retornar un arreglo de imgs y el loading en false', async () => {
+        const category = 'Ergo Proxy';
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs(category) );
+        await waitForNextUpdate();
+        const { data, loading } = result.current;
+        
+        expect( data.length ).toBe( 10 );
+        expect( loading ).toBe( false );
     })
     
 })
